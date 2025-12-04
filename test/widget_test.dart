b724b4d12dd056655e183e2a683a62c655ca75a6
Cahuan <cahuangg@gmail.com>
2025-12-04 00:35:43 -0300

    Add files via upload

---
+++ test/widget_test.dart
@@ -0,0 +1,9 @@
+// widget_test.dart
+import 'package:flutter_test/flutter_test.dart';
+
+void main() {
+  testWidgets('Teste básico do aplicativo', (WidgetTester tester) async {
+    // Testes podem ser adicionados aqui posteriormente
+    expect(1 + 1, equals(2));
+  });
+}
